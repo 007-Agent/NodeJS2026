10e7b5fb6eca0486452e10c79f7fbd00db810095
Абаничев Даниил Александрович <AbanichevDaniil@kdpmc.ru>
2026-01-16 15:01:58 +0300

    122

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -9,10 +9,35 @@ export async function getProducts (req, res) {
   try {
     const result = await pool.query('SELECT * FROM products');
     console.log(result, "RRRRR")
-    const baseUrl = 'http://10.16.1.66:3000'; // Замените на ваш URL сервера, если нужно
+    const baseUrl = 'http://10.16.1.156:3000'; // Замените на ваш URL сервера, если нужно
     const products = result.rows.map(product => ({
       ...product,
       photo_path: `${baseUrl}/${product.photo_path}`, // Полный URL изображения
+    }));
+   console.log(products, "PPRPR")
+    res.json({ success: true, data: products });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: 'Database error' });
+  }
+};
+
+export async function getProductCategory (req, res) {
+  try {
+    const { categoryId } = req.body;
+      console.log('Получен categoryId:', categoryId);
+     if (![1, 2, 3].includes(categoryId)) {
+        return res.status(400).json({
+          success: false,
+          error: 'Invalid category ID. Must be 1, 2, or 3'
+        });
+      }
+   const result = await pool.query('SELECT * FROM get_products_by_category_func($1)', [categoryId]);
+    console.log(result, "RRRRR")
+    const baseUrl = 'http://10.16.1.156:3000'; // Замените на ваш URL сервера, если нужно
+    const products = result.rows.map(product => ({
+      ...product,
+      main_image: `${baseUrl}${product.main_image}`, // Полный URL изображения
     }));
    console.log(products, "PPRPR")
     res.json({ success: true, data: products });
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import multer from 'multer';
 import { handleAddUser} from './controllers/UserControllers.js';
 import { handlegetUsers } from './controllers/UserControllers.js';
 import { getProducts } from './controllers/ProductsController.js';
+import { getProductCategory } from './controllers/ProductsController.js';
 import cors from 'cors';
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
-
+app.post('/api/products/all', getProductCategory)
 const upload = multer({dest : 'uploads/'})
 
 app.get('/test-db', async (req, res) => {
